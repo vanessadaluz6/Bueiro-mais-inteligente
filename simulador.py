953f21a315aafa8792aa243f01300bf13a8d73af
Lucas Veloso <lucasvelosobp@gmail.com>
2020-01-18 16:20:45 -0300

    Atualizei o download

--- simulador.py
+++ simulador.py
@@ -2,5 +2,5 @@ import requests
 
 while(True):
    valor = int(input("Informe um valor: "))
-   r = requests.post("http://bueiro-verde.herokuapp.com/upload", json={"LDR":valor})
+   r = requests.post("http://bueiro-verde.herokuapp.com/download", json={"LDR":valor})
    print(r.status_code)
