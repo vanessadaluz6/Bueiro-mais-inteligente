b96adc95677e09b239a5a2cfbca774f081f3bb32
Lucas Veloso <lucasvelosobp@gmail.com>
2020-01-22 09:52:45 -0300

    Comecei a fazer o layout da pagina

--- Bueiro/simulador.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-while(True):
-   valor = int(input("Informe um valor: "))
-   r = requests.post("http://bueiro-verde.herokuapp.com/upload", json={"LDR":valor})
-   print(r.status_code)
